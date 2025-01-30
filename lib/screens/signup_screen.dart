9f4706fd732bf4c6cacfced41bb0e7b72186560e
Hirusha Gunasena <101941577+TrippyHouseProductions@users.noreply.github.com>
2025-01-31 03:46:26 +0530

    created signup screen, updated login screen references

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,43 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:luxnewyork_flutter_app/screens/main_screen.dart';
-import 'package:luxnewyork_flutter_app/screens/signup_screen.dart';
-import 'package:luxnewyork_flutter_app/screens/forgot_password_screen.dart';
+import 'package:luxnewyork_flutter_app/screens/login_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
 
   @override
   // ignore: library_private_types_in_public_api
-  _LoginScreenState createState() => _LoginScreenState();
+  _SignupScreenState createState() => _SignupScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignupScreenState extends State<SignupScreen> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   bool _isPasswordVisible = false;
 
+  //ANCHOR - Use of notifications
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const MainScreen()),
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Sign Up Successful! Please Login')),
       );
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.pushReplacement(
+          // ignore: use_build_context_synchronously
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+        );
+      });
     }
-  }
-
-  void _navigateToSignup() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const SignupScreen()),
-    );
-  }
-
-  void _navigateToForgotPassword() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const ForgotPasswordScreen()),
-    );
   }
 
   @override
@@ -45,21 +39,26 @@ class _LoginScreenState extends State<LoginScreen> {
     final theme = Theme.of(context);
 
     return Scaffold(
+      appBar: AppBar(backgroundColor: theme.colorScheme.surface),
       body: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
           child: Form(
             key: _formKey,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 _buildTitle(theme),
+                _buildTextField("Full Name", _nameController,
+                    "Please enter your full name"),
                 _buildEmailField(),
-                _buildPasswordField(),
-                _buildForgotPassword(),
-                _buildLoginButton(),
-                _buildSignupOption(),
+                _buildPasswordField("Password", _passwordController),
+                _buildPasswordField(
+                    "Confirm Password", _confirmPasswordController,
+                    confirm: true),
+                const SizedBox(height: 20),
+                _buildSignupButton(),
+                _buildLoginOption(),
               ],
             ),
           ),
@@ -72,11 +71,29 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget _buildTitle(ThemeData theme) {
     return Column(
       children: [
-        Text("Sign In", style: theme.textTheme.headlineLarge),
+        Text("Sign Up", style: theme.textTheme.headlineLarge),
         const SizedBox(height: 8),
-        Text("Hi! Welcome back, you’ve been missed",
+        Text("Create an account to get started",
             style: theme.textTheme.bodyMedium),
         const SizedBox(height: 24),
+      ],
+    );
+  }
+
+  // Text Field
+  Widget _buildTextField(
+      String label, TextEditingController controller, String errorMessage) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(label),
+        const SizedBox(height: 0),
+        TextFormField(
+          controller: controller,
+          decoration: const InputDecoration(),
+          validator: (value) => value!.isEmpty ? errorMessage : null,
+        ),
+        const SizedBox(height: 16),
       ],
     );
   }
@@ -96,7 +113,7 @@ class _LoginScreenState extends State<LoginScreen> {
             if (value == null || value.isEmpty) {
               return "Please enter your email";
             }
-            // email validation
+            // email validation //ANCHOR - Use of email validation
             final emailRegex =
                 RegExp(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
             if (!emailRegex.hasMatch(value)) {
@@ -110,15 +127,16 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  // Password Field //ANCHOR - Use of simple validation
-  Widget _buildPasswordField() {
+  // Password Field
+  Widget _buildPasswordField(String label, TextEditingController controller,
+      {bool confirm = false}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text("Password"),
-        const SizedBox(height: 0), //5 looked good
+        Text(label),
+        const SizedBox(height: 0),
         TextFormField(
-          controller: _passwordController,
+          controller: controller,
           obscureText: !_isPasswordVisible,
           decoration: InputDecoration(
             suffixIcon: IconButton(
@@ -128,34 +146,27 @@ class _LoginScreenState extends State<LoginScreen> {
                   setState(() => _isPasswordVisible = !_isPasswordVisible),
             ),
           ),
+          //ANCHOR - Use of password validation
           validator: (value) {
             if (value == null || value.isEmpty) {
-              return "Please enter your password";
+              return "Please enter a password";
             }
             if (value.length < 6) {
               return "Password must be at least 6 characters";
             }
+            if (confirm && value != _passwordController.text) {
+              return "Passwords do not match";
+            }
             return null;
           },
         ),
-        const SizedBox(height: 10),
+        const SizedBox(height: 16),
       ],
     );
   }
 
-  // Forgot Password //ANCHOR - Use of linking
-  Widget _buildForgotPassword() {
-    return Align(
-      alignment: Alignment.centerRight,
-      child: TextButton(
-        onPressed: _navigateToForgotPassword,
-        child: const Text("Forgot Password?"),
-      ),
-    );
-  }
-
-  // Login Button
-  Widget _buildLoginButton() {
+  // Sign Up Button
+  Widget _buildSignupButton() {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
@@ -164,21 +175,21 @@ class _LoginScreenState extends State<LoginScreen> {
           padding: const EdgeInsets.symmetric(vertical: 14),
           textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         ),
-        child: const Text("Login"),
+        child: const Text("Sign Up"),
       ),
     );
   }
 
-  // Signup Option //ANCHOR - Use of linking
-  Widget _buildSignupOption() {
+  // Login Option //ANCHOR - Use of linking
+  Widget _buildLoginOption() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Text("Don’t have an account?",
+        const Text("Already have an account?",
             style: TextStyle(fontSize: 12, color: Colors.grey)),
         TextButton(
-          onPressed: _navigateToSignup,
-          child: const Text("Sign Up",
+          onPressed: () => Navigator.pop(context),
+          child: const Text("Login",
               style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
         ),
       ],
