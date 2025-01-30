adaa2769a30f8ad7f72318d5c7f838b0a7177bbe
Hirusha Gunasena <101941577+TrippyHouseProductions@users.noreply.github.com>
2025-01-31 04:05:31 +0530

    created wishlist screen, updated wishlist screen import references

---
+++ lib/screens/wishlist_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:luxnewyork_flutter_app/screens/main_screen.dart';
+
+class WishlistScreen extends StatelessWidget {
+  const WishlistScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Empty Wishlist Message
+          Text("Your Wishlist is Empty!",
+              style: Theme.of(context).textTheme.bodyMedium),
+          const SizedBox(height: 20), // Spacing
+
+          // Continue Shopping Button
+          ElevatedButton(
+            onPressed: () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const MainScreen()),
+            ),
+            child: const Text("Continue Shopping"),
+          ),
+        ],
+      ),
+    );
+  }
+}
