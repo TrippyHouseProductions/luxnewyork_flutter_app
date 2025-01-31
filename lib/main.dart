208a76107d53a4c00699cbd194e0c16ded20a72d
Hirusha Gunasena <101941577+TrippyHouseProductions@users.noreply.github.com>
2025-01-31 15:21:06 +0530

    reset main.dart after testing purposes

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme, // Light theme
       darkTheme: darkTheme, // Dark theme
       themeMode: ThemeMode.system, // Use system theme
-      home: const MainScreen(),
+      home: const SplashScreen(),
     );
   }
 }
