f81e05d9421221b732618e963c9640397ee24121
Hirusha Gunasena <101941577+TrippyHouseProductions@users.noreply.github.com>
2025-01-31 13:20:12 +0530

    removed commented code

--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -1,34 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:luxnewyork_flutter_app/screens/main_screen.dart';
-
-// class WishlistScreen extends StatelessWidget {
-//   const WishlistScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         mainAxisSize: MainAxisSize.min,
-//         children: [
-//           // Empty Wishlist Message
-//           Text("Your Wishlist is Empty!",
-//               style: Theme.of(context).textTheme.bodyMedium),
-//           const SizedBox(height: 20), // Spacing
-
-//           // Continue Shopping Button
-//           ElevatedButton(
-//             onPressed: () => Navigator.pushReplacement(
-//               context,
-//               MaterialPageRoute(builder: (context) => const MainScreen()),
-//             ),
-//             child: const Text("Continue Shopping"),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:luxnewyork_flutter_app/screens/main_screen.dart';
 import 'package:luxnewyork_flutter_app/models/product_data.dart'; // Import your product data
