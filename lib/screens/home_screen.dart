daef173f4a022ec99f50b84957bcfc6985933705
Hirusha Gunasena <hirushagunasena@gmail.com>
2025-06-04 16:08:09 +0530

    Adjust grid columns in landscape

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -89,12 +89,18 @@ class HomeScreen extends StatelessWidget {
 
   // Product Grid - //ANCHOR - Use of scrolable list
   Widget _buildProductGrid(BuildContext context) {
+    // Determine current orientation
+    Orientation orientation = MediaQuery.of(context).orientation;
+
+    // Set crossAxisCount based on orientation
+    int crossAxisCount = orientation == Orientation.portrait ? 2 : 4;
+
     return GridView.builder(
       shrinkWrap: true,
-      physics: const BouncingScrollPhysics(), // Adds smooth scrolling effect
+      physics: const BouncingScrollPhysics(),
       itemCount: products.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         childAspectRatio: 0.6,
