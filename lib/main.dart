20469d9e462a8ecba9c91f9e0ca37575145a8787
Hirusha Gunasena <hirushagunasena@gmail.com>
2025-06-07 03:41:04 +0530

    Remove no internet overlay

--- lib/main.dart
+++ lib/main.dart
@@ -54,8 +54,6 @@ import 'theme/theme.dart';
 import 'providers/cart_provider.dart';
 import 'providers/wishlist_provider.dart';
 import 'providers/theme_provider.dart';
-import 'providers/connectivity_provider.dart';
-import 'widgets/no_internet_overlay.dart';
 
 void main() {
   runApp(
@@ -64,7 +62,6 @@ void main() {
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(create: (_) => WishlistProvider()),
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ChangeNotifierProvider(create: (_) => ConnectivityProvider()),
       ],
       child: const MyApp(),
     ),
@@ -82,9 +79,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: themeProvider.themeMode,
-      home: const SplashScreen(),
-      builder: (context, child) =>
-          NoInternetOverlay(child: child ?? const SizedBox()),
+      home: const SplashScreen(), // The splash screen can decide login vs home
     );
   }
 }
